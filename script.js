49d834a9fd514c9581ffaa0d7a670d59c9d718e1
daimondcat <128719303+bigmonke1@users.noreply.github.com>
2024-03-26 12:30:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function onButtonClick() {
+    alert('ha your ip is 127.0.0.1:5050');
+  }
+  
+  const button = document.querySelector('button');
+  button.addEventListener('click', onButtonClick);
